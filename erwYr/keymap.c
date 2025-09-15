842b5e61739a9f7cd38987a9276ec87e4d1e3714
taon93 <m.nabialek.93@gmail.com>
2025-09-15 13:26:30 +0200

    adds repeat combo

--- erwYr/keymap.c
+++ erwYr/keymap.c
@@ -219,7 +219,7 @@ const uint16_t PROGMEM combo15[] = { KC_H, KC_K, COMBO_END};
 const uint16_t PROGMEM combo16[] = { MT(MOD_RALT, KC_5), MT(MOD_RGUI, KC_2), COMBO_END};
 const uint16_t PROGMEM combo17[] = { KC_9, KC_6, COMBO_END};
 const uint16_t PROGMEM combo18[] = { LALT(LGUI(KC_V)), LGUI(KC_V), COMBO_END};
-
+static const uint16_t COMBO_COUNT = COMBO_COUNT + 1;
 combo_t key_combos[COMBO_COUNT] = {
     COMBO(repeat, QK_REP),
     COMBO(combo0, KC_BSPC),
